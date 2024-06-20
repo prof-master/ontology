3cebf800ee1c36ea06f684fa6120321183646b8f
Alevtina Bazikalova <bazikalova.a@mail.ru>
2024-06-20 23:56:42 +0300

    Добавлены классы базы данных для предиката, субъекта и объекта

---
+++ ontology_constructor/ontology_auth/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Subject(models.Model):
+    name=models.CharField()
+    uri=models.CharFied()
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.uri
+class Object(models.Model):
+    name=models.CharField()
+    uri=models.CharFied()
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.uri
+class rdf_type(models.Model):
+    name=models.CharField()
+    uri=models.CharFied()
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.uri
