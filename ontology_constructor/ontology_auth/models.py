d84fc231c4ab9c5f43e7945b1be574f564a7b5da
Alevtina Bazikalova <bazikalova.a@mail.ru>
2024-06-22 13:34:11 +0300

    добавление моделей

--- ontology_constructor/ontology_auth/models.py
+++ ontology_constructor/ontology_auth/models.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 from django.db import models
-
+'''
 # Create your models here.
 class Subject(models.Model):
     name=models.CharField()
@@ -19,6 +18,18 @@ class Object(models.Model):
     def __str__(self):
         return self.uri
 class rdf_type(models.Model):
+    subject=models.ForeignKey(
+        Subject,
+        related_name='subject',
+        on_delete=models.CASCADE
+
+    )
+    object=models.ForeignKey(
+        Object,
+        related_name='object',
+        on_delete=models.CASCADE
+
+    )
     name=models.CharField()
     uri=models.CharFied()
     description=models.TextField()
@@ -26,8 +37,4 @@ class rdf_type(models.Model):
 
     def __str__(self):
         return self.uri
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> 3959197b9392de9167a19270e55166effc7736b7
+'''
